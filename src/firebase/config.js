0eb10680d19f1e068826663710c58635144f7b5f
Abdul-Farshad <abdulfarshad13@gmail.com>
2024-07-11 12:48:23 +0530

    Initial commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYTBsLREXYuAkfyjK1weuGJ72MwCdczHE",
-  authDomain: "olx-clone-1b2a1.firebaseapp.com",
-  projectId: "olx-clone-1b2a1",
-  storageBucket: "olx-clone-1b2a1.appspot.com",
-  messagingSenderId: "787328249432",
-  appId: "1:787328249432:web:f134565ec4ed7cea5a242d",
-  measurementId: "G-MQFETMZSMB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT_ID
 };
 
 const firebase = initializeApp(firebaseConfig);
